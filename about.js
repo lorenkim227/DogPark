d50d1303e80882c4a44a6116aac90d15f3a06b84
lorenkim <lkim1@loyola.edu>
2026-02-26 11:21:51 -0500

    buttons work, making UI pretty

--- about.js
+++ about.js
@@ -1,29 +1,17 @@
 function showPool() {
-    var poolInfo = document.getElementById("pool");
-    var playAreaInfo = document.getElementById("play");
-    var agilityCourseInfo = document.getElementById("agility");
-
-    document.getElementById(poolInfo).setAttribute("style", "visibility: visible; display: block;");
-    document.getElementById(playAreaInfo).setAttribute("style", "visibility: hidden; display: none;");
-    document.getElementById(agilityCourseInfo).setAttribute("style", "visibility: hidden; display: none;");
+    document.getElementById("pool").setAttribute("style", "visibility: visible; display: block;");
+    document.getElementById("play").setAttribute("style", "visibility: hidden; display: none;");
+    document.getElementById("agility").setAttribute("style", "visibility: hidden; display: none;");
 }
 
 function showPlayArea() {
-    var poolInfo = document.getElementById("pool");
-    var playAreaInfo = document.getElementById("play");
-    var agilityCourseInfo = document.getElementById("agility");
-
-    document.getElementById(poolInfo).setAttribute("style", "visibility: hidden; display: none;");
-    document.getElementById(playAreaInfo).setAttribute("style", "visibility: visible; display: block;");
-    document.getElementById(agilityCourseInfo).setAttribute("style", "visibility: hidden; display: none;");
+    document.getElementById("pool").setAttribute("style", "visibility: hidden; display: none;");
+    document.getElementById("play").setAttribute("style", "visibility: visible; display: block;");
+    document.getElementById("agility").setAttribute("style", "visibility: hidden; display: none;");
 }
 
 function showAgilityCourse() {
-    var poolInfo = document.getElementById("pool");
-    var playAreaInfo = document.getElementById("play");
-    var agilityCourseInfo = document.getElementById("agility");
-
-    document.getElementById(poolInfo).setAttribute("style", "visibility: hidden; display: none;");
-    document.getElementById(playAreaInfo).setAttribute("style", "visibility: hidden; display: none;");
-    document.getElementById(agilityCourseInfo).setAttribute("style", "visibility: visible; display: block;");
+    document.getElementById("pool").setAttribute("style", "visibility: hidden; display: none;");
+    document.getElementById("play").setAttribute("style", "visibility: hidden; display: none;");
+    document.getElementById("agility").setAttribute("style", "visibility: visible; display: block;");
 }
